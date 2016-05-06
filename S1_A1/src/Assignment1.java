7503efd045fc57be5256ba770b3a2521d9e6a5b3
pkatiyar13 <katiyar.cool@gmail.com>
2016-05-06 12:18:48 +0530

    Java Session1 Assignment 1

---
+++ S1_A1/src/Assignment1.java
@@ -0,0 +1,11 @@
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		float b = 20;
+		System.out.println("Sum of Integer Number and Float Number is: "+(a+b));
+	}
+
+}
